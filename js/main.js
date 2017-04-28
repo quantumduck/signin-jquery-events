af8ebcd0916eb14dec9523509a0485d073f5c6cc
quantumduck <quantum.duck@hotmail.com>
2017-04-28 14:23:27 -0400

    function to find last argument

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
   flashMessage('Ready!');
-  form = $('.modal').first();
+  formTags = $('.modal, .modal *');
 
 });
 
@@ -11,4 +11,28 @@ function flashMessage(message) {
   setTimeout(function() {
     status.text(originalText);
   }, 500);
+}
+
+function fadeInTags(tags) {
+  for (var i = 0; i < formTags.length; i++) {
+    var finalBGColor = formTags[i].style['background-color'];
+    var finalTransparency = 1;
+    if (finalBGColor.substring(0,4) === "rgba") {
+      finalTransparency = lastArgument(finalBGColor);
+    }
+  }
+}
+
+function lastArgument(string) {
+  // Return one argument from a function string
+  while (string.indexOf(',') >= 0) {
+    string = string.substring(string.indexOf(',') + 1, string.length);
+  }
+  while (string[0] === ' ') {
+    string = string.substring(1, string.length);
+  }
+  if (string[string.length - 1] === ')') {
+    string = string.substring(0, string.length - 1);
+  }
+  return string;
 }
