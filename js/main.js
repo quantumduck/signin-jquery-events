6426dbc719466a46efc46cfff8f932ef452c6954
quantumduck <quantum.duck@hotmail.com>
2017-04-28 12:14:10 -0400

    verified js loading

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(function() {
-  
+  flashMessage('Ready!');
+  form = $('.modal').first();
 
 });
 
 function flashMessage(message) {
-  var status = $('.status').first();
+  var status = $('.hello').first();
   var originalText = status.text();
   status.text(message);
   setTimeout(function() {
